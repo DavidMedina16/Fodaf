0bec998e677f239ae0a37ac80bd351fa8528b859
davidparramedina <davidparramedina@hotmail.com>
2026-01-07 12:12:22 -0500

    fix(frontend): add value input binding to input and select components

--- frontend/src/app/shared/components/select/select.component.ts
+++ frontend/src/app/shared/components/select/select.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output, forwardRef, signal } from '@angular/core';
+import { Component, input, output, forwardRef, signal, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormsModule } from '@angular/forms';
 
@@ -27,11 +27,21 @@ export class SelectComponent implements ControlValueAccessor {
   placeholder = input<string>('Seleccionar...');
   disabled = input<boolean>(false);
   hasError = input<boolean>(false);
+  initialValue = input<string | number>('', { alias: 'value' });
 
   selectionChange = output<string | number>();
 
   value = signal<string | number>('');
   isFocused = signal<boolean>(false);
+
+  constructor() {
+    effect(() => {
+      const initial = this.initialValue();
+      if (initial !== undefined) {
+        this.value.set(initial);
+      }
+    });
+  }
 
   private onChange: (value: string | number) => void = () => {};
   private onTouched: () => void = () => {};

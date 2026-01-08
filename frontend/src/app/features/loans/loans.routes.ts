5e0a2655056eb2e3437197751d9db7588c06cb34
davidparramedina <davidparramedina@hotmail.com>
2026-01-08 14:29:16 -0500

    feat(frontend): configure loans module routes

--- frontend/src/app/features/loans/loans.routes.ts
+++ frontend/src/app/features/loans/loans.routes.ts
@@ -4,5 +4,13 @@ export const LOANS_ROUTES: Routes = [
   {
     path: '',
     loadComponent: () => import('./loans-list/loans-list.component').then(m => m.LoansListComponent)
+  },
+  {
+    path: 'new',
+    loadComponent: () => import('./loan-request/loan-request.component').then(m => m.LoanRequestComponent)
+  },
+  {
+    path: ':id',
+    loadComponent: () => import('./loan-detail/loan-detail.component').then(m => m.LoanDetailComponent)
   }
 ];

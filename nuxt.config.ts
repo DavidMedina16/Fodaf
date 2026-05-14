e22d3088e223fce4de90be0110b0dcf2fc3a70a9
davidparramedina <davidparramedina@hotmail.com>
2026-05-14 09:47:06 -0500

    chore: print custom dev host url in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,4 +16,12 @@ export default defineNuxtConfig({
       supabaseKey: process.env.SUPABASE_KEY || '',
     },
   },
+
+  hooks: {
+    listen(_server, listener) {
+      const port = listener?.address?.port ?? 4111
+      // eslint-disable-next-line no-console
+      console.log(`  \x1B[36m➜\x1B[0m Hosts:    \x1B[36mhttp://fodaf.development:${port}/\x1B[0m`)
+    },
+  },
 })
